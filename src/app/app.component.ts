d44ce0042c022494679f89c6e54cb3ffc21ef136
YayaSOW <yaya.sow@ism.edu.sn>
2026-04-28 00:06:07 +0200

    feat: add pagination to SwapiService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,28 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { AgGridModule } from 'ag-grid-angular';
 import { SwapiService } from './services/swapi.service';
 import { Starship } from './models/starship.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AgGridModule],
+  imports: [AgGridModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
 
   starships: Starship[] = [];
+  isLoading: boolean = true; // 
+  errorMessage: string | null = null;
 
-  constructor(private swapiService: SwapiService) {}
+  constructor(private swapiService: SwapiService) { }
 
   ngOnInit(): void {
-    this.swapiService.getStarships(1).subscribe({
-      next: (data) => {
-        console.log('SWAPI DATA:', data);
-        this.starships = data.results;
+    this.swapiService.loadFirstPage().subscribe({
+      next: (response) => {
+        this.swapiService.setNextPageUrl(response.next);
+        this.starships = response.results;
+        this.isLoading = false;
       },
       error: (err) => {
         console.error('Erreur API:', err);
+        this.errorMessage = 'Failed to load starships. Please try again later.';
+        this.isLoading = false;
       }
     });
   }
